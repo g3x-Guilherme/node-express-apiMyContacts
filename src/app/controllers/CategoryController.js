250ef832be834b63d5994ad14578230b4a0750da
g3xnegative1 <pedrog3x7@gmail.com>
2022-08-02 01:35:51 -0300

    creating category module

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -7,6 +7,27 @@ class CategoryController {
     response.json(categories);
   }
 
+  async show(request, response) {
+    const { id } = request.params;
+
+    const category = await CategoriesRepository.findById(id);
+
+    if (!category) {
+      // 404 not found
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoriesRepository.delete(id);
+
+    response.sendStatus(204);
+  }
+
   async store(request, response) {
     const { name } = request.body;
 
@@ -17,6 +38,10 @@ class CategoryController {
     const category = await CategoriesRepository.create({ name });
 
     response.json(category);
+  }
+
+  update(request, response) {
+    response.json({ message: 'update' });
   }
 }
 
